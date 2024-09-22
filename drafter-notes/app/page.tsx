27e1d38bcd8ed70565708c568bb26bb9371a3567
Mary Songal <songalmary@gmail.com>
2024-09-22 12:27:58 +0200

    000: Initial project setup

---
+++ drafter-notes/app/page.tsx
@@ -0,0 +1,11 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        This is initial clean project. Main development is in a feature branch.
+      </main>
+    </div>
+  );
+}
